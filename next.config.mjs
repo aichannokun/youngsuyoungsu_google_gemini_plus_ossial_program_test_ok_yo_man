a83a6d72fee80dc731094ac0899cfc72a6bcd551
aichannokun <aichannokun@gmail.com>
2026-04-03 10:57:38 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Turbopack 관련 경고가 날 경우 아래 옵션이 도움이 됩니다
+  devIndicators: {
+    appIsrStatus: false,
+  },
 };
+
 export default nextConfig;
